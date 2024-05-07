289456e78bf3f56af548c1616dc8467596d23c8b
Stefano Cecere <stefano.cecere@krur.com>
2024-05-07 18:00:36 +0200

    init

---
+++ projectData/HomerVars.js
@@ -0,0 +1,105 @@
+/**
+* javascript variables for the Homer project:
+* FEM+PLAY - Mondo Futuro
+* Homer - The Story Flow Editor.
+* Copyright (c)2021-2024. Open Lab s.r.l - Florence, Italy
+* Developer: Pupunzi (Matteo Bicocchi)
+*
+**/
+
+class HomerVars {
+
+      static world_temperature = 28;
+
+};
+
+class HomerActors {
+
+      static NARRATORE = "Narratore";
+
+}
+
+class HomerMeta {
+      static FLOW_STATE = {
+<br />
+<b>Warning</b>:  Undefined property: stdClass::$_type in <b>/var/www/vhosts/homer.open-lab.com/httpdocs/php/out/js.php</b> on line <b>87</b><br />
+          IDEA : 'IDEA',
+          NOTES : 'NOTES',
+          DRAFT : 'DRAFT',
+          EDITOR : 'EDITOR',
+          FINAL : 'FINAL',
+      };
+      static MOOD = {
+<br />
+<b>Warning</b>:  Undefined property: stdClass::$_type in <b>/var/www/vhosts/homer.open-lab.com/httpdocs/php/out/js.php</b> on line <b>87</b><br />
+          NEUTRAL : 'NEUTRAL',
+          HAPPY : 'HAPPY',
+          VERY_HAPPY : 'VERY_HAPPY',
+          SAD : 'SAD',
+          ANGRY : 'ANGRY',
+          AMAZED : 'AMAZED',
+      };
+      static EXPRESSION = {
+<br />
+<b>Warning</b>:  Undefined property: stdClass::$_type in <b>/var/www/vhosts/homer.open-lab.com/httpdocs/php/out/js.php</b> on line <b>87</b><br />
+          RELAXED : 'RELAXED',
+          GRINNING : 'GRINNING',
+          PENSIVE : 'PENSIVE',
+          FROWNING : 'FROWNING',
+          CRYING : 'CRYING',
+          ENRAGED : 'ENRAGED',
+      };
+      static BALLOON_TYPE = {
+<br />
+<b>Warning</b>:  Undefined property: stdClass::$_type in <b>/var/www/vhosts/homer.open-lab.com/httpdocs/php/out/js.php</b> on line <b>87</b><br />
+          SPEECH : 'SPEECH',
+          WHISPER : 'WHISPER',
+          THOUGHT : 'THOUGHT',
+          SCREAM : 'SCREAM',
+          SLEEP : 'SLEEP',
+          SING_SONG : 'SING_SONG',
+      };
+      static LOCATION = {
+<br />
+<b>Warning</b>:  Undefined property: stdClass::$_type in <b>/var/www/vhosts/homer.open-lab.com/httpdocs/php/out/js.php</b> on line <b>87</b><br />
+          HOME : 'HOME',
+          OUTSIDE : 'OUTSIDE',
+          OFFICE : 'OFFICE',
+          PARK : 'PARK',
+          SEA : 'SEA',
+          MOUNTAIN : 'MOUNTAIN',
+      };
+      static CAMERA_DIRECTION = {
+<br />
+<b>Warning</b>:  Undefined property: stdClass::$_type in <b>/var/www/vhosts/homer.open-lab.com/httpdocs/php/out/js.php</b> on line <b>87</b><br />
+          CLOSE_UP : 'CLOSE_UP',
+          ANGLE_ON : 'ANGLE_ON',
+          OFF_SCREEN : 'OFF_SCREEN',
+          FADE_OUT : 'FADE_OUT',
+          FADE_IN : 'FADE_IN',
+          VOICE_OVER : 'VOICE_OVER',
+      };
+      static LIGHTNING = {
+<br />
+<b>Warning</b>:  Undefined property: stdClass::$_type in <b>/var/www/vhosts/homer.open-lab.com/httpdocs/php/out/js.php</b> on line <b>87</b><br />
+          KEY : 'KEY',
+          BACK : 'BACK',
+          PRACTICAL : 'PRACTICAL',
+          HARD : 'HARD',
+          SOFT : 'SOFT',
+          CHIAROSCURO : 'CHIAROSCURO',
+      };
+
+}
+
+class HomerLabels {
+
+
+}
+
+
+class HomerFlowSlugs {
+      static LOOP_SCENE = "Loop scene";
+      static START = "Start";
+ }
+
